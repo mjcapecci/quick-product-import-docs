3d81b012ee878f7ed56569a439287916ce389a38
Michael Capecci <mjcapecci@gmail.com>
2021-03-09 09:49:53 -0600

    adds contact form reception logic

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../styles/form.scss';
 
 const ContactForm = () => {
@@ -11,8 +12,20 @@ const ContactForm = () => {
     });
   };
 
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    axios.post('/', {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact-form', formData }),
+    });
     setFormData({
       name: '',
       email: '',
@@ -22,29 +35,36 @@ const ContactForm = () => {
 
   return (
     <>
-      <form>
+      <form
+        name="contact-form"
+        method="POST"
+        data-netlify="true"
+        action="/thank-you"
+      >
         <input
-          type='text'
-          name='name'
-          placeholder='Name'
+          type="text"
+          name="name"
+          placeholder="Name"
           onChange={updateInput}
           value={formData.name || ''}
         />
         <input
-          type='email'
-          name='email'
-          placeholder='Email'
+          type="email"
+          name="email"
+          placeholder="Email"
           onChange={updateInput}
           value={formData.email || ''}
         />
         <textarea
-          type='text'
-          name='message'
-          placeholder='Message'
+          type="text"
+          name="message"
+          placeholder="Message"
           onChange={updateInput}
           value={formData.message || ''}
         ></textarea>
-        <button type='submit'>Submit</button>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
       </form>
     </>
   );
